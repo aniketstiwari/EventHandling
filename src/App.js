b242dc82120daf4c216aa7c8a4489ef3f6059e4e
anikett-rpx <anikett_c@rpxcorp.com>
2020-09-24 17:35:24 +0530

    Event handling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+const App = () => {
+  const purple = '#8e44ad';
+  const [bg, changeBgState] = useState(purple);
+  const initext = 'click me';
+  const [newtext, textState] = useState(initext);
+
+  const bgchange = () => {
+    let newbg = '#34495e';
+    changeBgState(newbg);
+    textState('text change 🛩️')
+  }
+
+  const bgBack = () => {
+    changeBgState(purple);
+    textState(initext)
+  }
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div style={{backgroundColor: bg}}>
+        <button onClick={bgchange} onDoubleClick={bgBack}>{newtext}</button>
+      </div>
+    </>
+  )
 }
+
 
 export default App;
